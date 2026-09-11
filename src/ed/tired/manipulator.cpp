ca49a767107c430978b7bbbd0c5a69c240438c18
Pavel Turapin <paveltrpn@integra-s.com>
2026-09-11 19:08:49 +0400

    update

--- src/ed/tired/manipulator.cpp
+++ src/ed/tired/manipulator.cpp
@@ -21,6 +21,21 @@ auto Manipulator::trackball() -> const vsg::ref_ptr<Trackball> {
     return _trackball;
 }
 
+QVector3D Manipulator::eye() const {
+    return QVector3D{ static_cast<float>( _trackball->eye().x ), static_cast<float>( _trackball->eye().y ),
+                      static_cast<float>( _trackball->eye().z ) };
+}
+
+QVector3D Manipulator::center() const {
+    return QVector3D{ static_cast<float>( _trackball->center().x ), static_cast<float>( _trackball->center().y ),
+                      static_cast<float>( _trackball->center().z ) };
+}
+
+QVector3D Manipulator::up() const {
+    return QVector3D{ static_cast<float>( _trackball->up().x ), static_cast<float>( _trackball->up().y ),
+                      static_cast<float>( _trackball->up().z ) };
+}
+
 // ======================================================================================
 // ==================== Trackball =======================================================
 // ======================================================================================
@@ -32,6 +47,18 @@ Trackball::Trackball( vsg::ref_ptr<vsg::Camera> camera, vsg::ref_ptr<vsg::Ellips
 
 void Trackball::apply( vsg::MoveEvent& moveEvent ) {
     vsg::Trackball::apply( moveEvent );
+}
+
+auto Trackball::eye() const -> vsg::dvec3 {
+    return vsg::Trackball::_lookAt->eye;
+}
+
+auto Trackball::center() const -> vsg::dvec3 {
+    return vsg::Trackball::_lookAt->center;
+}
+
+auto Trackball::up() const -> vsg::dvec3 {
+    return vsg::Trackball::_lookAt->up;
 }
 
 }  // namespace tire
--- src/ed/tired/manipulator.h
+++ src/ed/tired/manipulator.h
@@ -24,16 +24,28 @@ struct Trackball;
 struct Manipulator final : QObject {
     Q_OBJECT
 
+    Q_PROPERTY( QVector3D eye READ eye NOTIFY eyeChanged FINAL )
+    Q_PROPERTY( QVector3D center READ center NOTIFY centerChanged FINAL )
+    Q_PROPERTY( QVector3D up READ up NOTIFY upChanged FINAL )
+
 public:
     Manipulator( vsg::ref_ptr<vsg::Camera> camera, vsg::ref_ptr<vsg::EllipsoidModel> ellipsoidModel = {},
                  QObject* parent = nullptr );
 
     auto trackball() -> const vsg::ref_ptr<Trackball>;
 
+    QVector3D eye() const;
+    QVector3D center() const;
+    QVector3D up() const;
+
+    friend Trackball;
+signals:
+    void eyeChanged();
+    void centerChanged();
+    void upChanged();
+
 private:
     vsg::ref_ptr<Trackball> _trackball{};
-
-    QPoint mousePos_{};
 };
 
 // ======================================================================================
@@ -44,6 +56,10 @@ struct Trackball final : public vsg::Trackball {
     Trackball( vsg::ref_ptr<vsg::Camera> camera, vsg::ref_ptr<vsg::EllipsoidModel> ellipsoidModel = {} );
 
     void apply( vsg::MoveEvent& moveEvent ) override;
+
+    auto eye() const -> vsg::dvec3;
+    auto center() const -> vsg::dvec3;
+    auto up() const -> vsg::dvec3;
 
 private:
 };
