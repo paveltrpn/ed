9f87e33f1aaac09cc8fe1f383d6400c19276dab3
Pavel Turapin <paveltrpn@mail.com>
2026-09-14 21:31:22 +0400

    update

--- src/ed/tired/subgraph/subgraph.h
+++ src/ed/tired/subgraph/subgraph.h
@@ -31,6 +31,9 @@ protected:
 
     // StateGroup is the root of the scene/command graph to hold the GraphicsPipeline, and
     // binding of Descriptors to decorate the whole graph.
+    // For example, a vsg::Geometry node alone only defines what to draw. To define
+    // how it is drawn (shaders, pipeline state), it must be a child of a vsg::StateGroup
+    // configured with the appropriate ShaderSet and pipeline states.
     vsg::ref_ptr<vsg::StateGroup> _stateGroup{};
 };
 
