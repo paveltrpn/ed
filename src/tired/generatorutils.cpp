05482691b217abf1d630142eb4435980f2e46dea
Pavel Turapin <paveltrpn@mail.com>
2026-09-18 21:31:31 +0400

    update

--- src/tired/generatorutils.cpp
+++ src/tired/generatorutils.cpp
@@ -2,5 +2,21 @@
 
 namespace tire {
 
+auto VsgMeshDataGenerator::box() -> VsgMeshData {
+    return {};
+}
+
+auto VsgMeshDataGenerator::sphere() -> VsgMeshData {
+ return {};
+}
+
+auto VsgMeshDataGenerator::cylinder() -> VsgMeshData {
+ return {};
+}
+
+auto VsgMeshDataGenerator::capsule() -> VsgMeshData {
+ return {};
+}
+
 }
 
--- src/tired/generatorutils.h
+++ src/tired/generatorutils.h
@@ -1,5 +1,34 @@
 #pragma once
 
+#include <vsg/all.h>
+
 namespace tire {
+
+// ======================================================================================
+// ==================== VsgMeshData =====================================================
+// ======================================================================================
+
+struct VsgMeshData final {
+    vsg::ref_ptr<vsg::vec3Array> vertices{};
+    vsg::ref_ptr<vsg::vec3Array> normals{};
+    vsg::ref_ptr<vsg::vec3Array> colors{};
+    vsg::ref_ptr<vsg::vec2Array> texcrds{};
+    vsg::ref_ptr<vsg::uintArray> indices{};
+};
+
+// ======================================================================================
+// ==================== VsgMeshDataGenerator ============================================
+// ======================================================================================
+
+struct VsgMeshDataGenerator final {
+public:
+    static auto box() -> VsgMeshData;
+    static auto sphere() -> VsgMeshData;
+    static auto cylinder() -> VsgMeshData;
+    static auto capsule() -> VsgMeshData;
+
+private:
+
+};
 
 }
